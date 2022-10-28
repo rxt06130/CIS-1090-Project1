f2aec1503473d366951ca1cf590fa379f06be8e2
Test Student <cuteststudent@billkuker.com>
2022-10-28 08:43:31 -0400

    Remove the interface

--- personal-dictionary.js
+++ personal-dictionary.js
@@ -1,17 +1,1 @@
-//An array of words, initially empty
-let words = [];
-
-//This function searches the words array for the word
-//parameter and returns true if it is found, false
-//otherwise
-function search(word){
-
-}
-
-//This function adds the provided word to the words
-//array
-function addWord(word){
-
-}
-
-export {search, addWord};
+//Fill in this empty file
