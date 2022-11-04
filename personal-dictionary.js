ba6dfcdc436adbd521c033f10d1796013f43525f
rxt06130 <rxt06130@castleton.edu>
2022-11-04 10:46:31 -0400

    project 1

--- personal-dictionary.js
+++ personal-dictionary.js
@@ -1,1 +1,20 @@
 //Fill in this empty file
+
+//array
+
+let words = [apple, banana, cantaloupe, grape, monkey, table, computer, dirt];
+
+                                                                      
+    // a linear search
+
+    function otherSearch(needle, haystack){    
+        let start = 0; 
+        let end = haystack.length; 
+        
+        for (let i = start; i < end; i++) {
+            if (needle == haystack[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
